c597fd8e34e9f43127757f8c0148847379eb5ee7
KRISHAN <krishandevraj8@gmail.com>
2024-07-15 12:28:44 +0530

    added responsiveness for mobile for timeline  section

--- src/components/core/HomePage/TimelineSection.jsx
+++ src/components/core/HomePage/TimelineSection.jsx
@@ -29,10 +29,10 @@ const timeline = [
 ];
 const TimelineSection = () => {
   return (
-    <div className="flex gap-16">
-      <div className="w-[40%] flex flex-col gap-5">
+    <div className="flex lg:flex-row flex-col gap-10 lg:gap-16 lg:my-20 my-10">
+      <div className="lg:w-[40%] w-full flex flex-col gap-5">
         {timeline.map((item, index) => (
-          <div className="flex flex-col">
+          <div className="flex flex-col" key={index}>
             <div key={index} className="flex items-center gap-4">
               <img
                 src={item.Logo}
@@ -47,7 +47,7 @@ const TimelineSection = () => {
               </div>
             </div>
               {timeline.length - 1 > index && (
-                <div className="w-[1px] h-[45px] bg-richblack-700 ml-5"></div>
+                <div className="w-[1px] hidden lg:block h-[45px] bg-richblack-25 ml-5"></div>
               )}
           </div>
         ))}
@@ -58,14 +58,14 @@ const TimelineSection = () => {
           alt="timelineImage"
           className="object-cover shadow-white h-fit"
         />
-        <div className="absolute bg-caribbeangreen-700 flex text-white uppercase py-7 left-[50%] -bottom-[15%] translate-x-[-50%] -translate-y-[50%] ">
-          <div className="flex gap-5 items-center border-r border-caribbeangreen-300 px-7">
+        <div className="absolute bg-caribbeangreen-700 flex lg:flex-row flex-col text-white uppercase py-7 lg:left-[50%] lg:-bottom-[15%] lg:translate-x-[-50%] lg:w-full w-[250px] lg:-translate-y-[50%] top-1/2 left-1/2 -translate-x-1/3 ">
+          <div className="flex lg:gap-5 gap-10 items-center border-r border-caribbeangreen-300 px-7  my-5">
             <p className="text-3xl font-bold">10</p>
             <p className="text-cari font-bold text-sm text-caribbeangreen-300">
               Years of experiance
             </p>
           </div>
-          <div className="flex gap-5 items-center px-7">
+          <div className="flex lg:gap-5 gap-10 items-center px-7 my-5">
             <p className="text-3xl font-bold">250</p>
             <p className="text-cari font-bold text-sm text-caribbeangreen-300">
               Type of courses
