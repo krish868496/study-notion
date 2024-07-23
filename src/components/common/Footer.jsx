436261705ce4f30233b4d28fcc2a59e424d4f091
KRISHAN <krishandevraj8@gmail.com>
2024-07-23 11:28:36 +0530

    added footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { FooterLink1 } from "../../data/footer-links";
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-richblack-800 text-richblack-100">
+      {FooterLink1.map(({ title, links, mediaLinks }, index) => {
+        return (
+          <div key={index} className="flex flex-col gap-4">
+            <h2 className="font-semibold text-lg leading-7 tracking-wide">
+              {title}
+            </h2>
+            <ul>
+              {links.map(({ title, link }, index) => (
+                <li key={index}>
+                  <a key={index} href={link} target="_blank" rel="noreferrer">
+                    {title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            {/* {
+            mediaLinks?.map((icon, index) => (
+              <div className="" key={index}>
+                {icon}
+              </div>
+            ))
+          } */}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Footer
