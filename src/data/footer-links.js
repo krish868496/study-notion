2e927e1d25129ae53e0ceed469aa6b4b03dd87f0
KRISHAN <krishandevraj8@gmail.com>
2024-07-26 10:39:47 +0530

    changes in footer link

--- src/data/footer-links.js
+++ src/data/footer-links.js
@@ -1,40 +1,107 @@
+import { FaFacebook } from 'react-icons/fa'
+import { BsGoogle, BsYoutube } from 'react-icons/bs'
+import { PiLinkedinLogo } from 'react-icons/pi'
+export const FooterLink1 = [
+        {
+                title: "Company",
+                links: [
+                        { title: "Home", link: "/ai" },
+                        { title: "About", link: "/ml" },
+                        { title: "Careers", link: "/dl" },
+                        { title: "Affliate", link: "/cv" },
+                ],
+                mediaLinks: [
+                        <FaFacebook />,
+                        <BsGoogle />,
+                        <PiLinkedinLogo />,
+                        <BsYoutube />
+                ]
+        },
+        {
+                title: "Resources",
+                links: [
+                        { title: "Bash", link: "/bash" },
+                        { title: "c++", link: "/c++" },
+                        { title: "c", link: "/c" },
+                        { title: "java", link: "/java" },
+                        { title: "javascript", link: "/javascript" },
+                        { title: "python", link: "/python" },
+                        { title: "php", link: "/php" },
+                        { title: "html", link: "/html" },
+                        { title: "css", link: "/css" },
+                        { title: "sql", link: "/sql" },
+                        { title: "ruby", link: "/ruby" },
+                        { title: "swift", link: "/swift" },
+                        { title: "go", link: "/go" },
+                        { title: "rust", link: "/rust" },
+                        { title: "kotlin", link: "/kotlin" },
+                        { title: "swift", link: "/swift" },
+                        { title: "swift", link: "/swift" },
+                ]
+        },
+        {
+                title: "Support",
+                links: [
+                        { title: "Blockchain", link: "/blockchain" },
+                        { title: "IoT", link: "/" },
+                        { title: "AI", link: "/" },
+                        { title: "ML", link: "/" },
+                        { title: "DL", link: "/" },
+                ]
+        }
+]
+
 export const FooterLink2 = [
         {
                 title: "Subjects",
                 links: [
-                        {title: "AI", link: "/ai"},
-                        {title: "ML", link: "/ml"},
-                        {title: "DL", link: "/dl"},
-                        {title: "CV", link: "/cv"},
-                        {title: "NLP", link: "/nlp"},
-                        {title: "DS", link: "/ds"},
-                        {title: "SE", link: "/se"},
-                        {title: "OS", link: "/os"},
-                        {title: "DBMS", link: "/dbms"},
-                        {title: "CN", link: "/cn"},
-                        {title: "DS", link: "/ds"},
+                        { title: "AI", link: "/ai" },
+                        { title: "ML", link: "/ml" },
+                        { title: "DL", link: "/dl" },
+                        { title: "CV", link: "/cv" },
+                        { title: "NLP", link: "/nlp" },
+                        { title: "DS", link: "/ds" },
+                        { title: "SE", link: "/se" },
+                        { title: "OS", link: "/os" },
+                        { title: "DBMS", link: "/dbms" },
+                        { title: "CN", link: "/cn" },
+                        { title: "DS", link: "/ds" },
                 ]
         },
         {
                 title: "Languages",
                 links: [
-                        {title: "Bash", link: "/bash"},
-                        {title: "c++", link: "/c++"},
-                        {title: "c", link: "/c"},
-                        {title: "java", link: "/java"},
-                        {title: "javascript", link: "/javascript"},
-                        {title: "python", link: "/python"},
-                        {title: "php", link: "/php"},
-                        {title: "html", link: "/html"},
-                        {title: "css", link: "/css"},
-                        {title: "sql", link: "/sql"},
-                        {title: "ruby", link: "/ruby"},
-                        {title: "swift", link: "/swift"},
-                        {title: "go", link: "/go"},
-                        {title: "rust", link: "/rust"},
-                        {title: "kotlin", link: "/kotlin"},
-                        {title: "swift", link: "/swift"},
-                        {title: "swift", link: "/swift"},
+                        { title: "Bash", link: "/bash" },
+                        { title: "c++", link: "/c++" },
+                        { title: "c", link: "/c" },
+                        { title: "java", link: "/java" },
+                        { title: "javascript", link: "/javascript" },
+                        { title: "python", link: "/python" },
+                        { title: "php", link: "/php" },
+                        { title: "html", link: "/html" },
+                        { title: "css", link: "/css" },
+                        { title: "sql", link: "/sql" },
+                        { title: "ruby", link: "/ruby" },
+                        { title: "swift", link: "/swift" },
+                        { title: "go", link: "/go" },
+                        { title: "rust", link: "/rust" },
+                        { title: "kotlin", link: "/kotlin" },
+                        { title: "swift", link: "/swift" },
+                        { title: "swift", link: "/swift" },
+                ]
+        },
+        {
+                title: "Technology",
+                links: [
+                        { title: "Blockchain", link: "/blockchain" },
+                        { title: "IoT", link: "/" },
+                        { title: "AI", link: "/" },
+                        { title: "ML", link: "/" },
+                        { title: "DL", link: "/" },
+                        { title: "CV", link: "/" },
+                        { title: "-", link: "" },
+                        { title: "DS", link: "/" },
+                        { title: "ALGO", link: "" }
                 ]
         }
 ]
