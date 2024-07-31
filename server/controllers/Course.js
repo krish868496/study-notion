d09168c291becd1bd97f5933a24702bf6b6369eb
KRISHAN <krishandevraj8@gmail.com>
2024-07-31 15:56:29 +0530

    changes on course contoller

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -3,28 +3,42 @@ const Course = require("../models/Course")
 const Category = require("../models/Category")
 const { uploadImageToCloudinary } = require("../utils/imageUploader")
 const User = require("../models/User")
-const { login } = require("./Auth")
 
 
 exports.createCourse = async (req, res) => {
         try {
                 // fetch data 
-                const { courseName, courseDescription, whatYouWillLearn, price, tag, category, instructions } = req.body;
+                const { courseName, courseDescription, whatYouWillLearn, price,
+                        tag,
+                        category,
+                        instructions,
+                        status
+                } = req.body;
                 // get thumbnail 
 
                 const { thumbnailImage } = req.files;
+                console.log(thumbnailImage, "Thumbnail");
 
-                if (!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !thumbnailImage || !category || !instructions) {
+                if (!courseName || !courseDescription || !whatYouWillLearn || !price
+                        || !tag 
+                        || !thumbnailImage
+                        || !category
+                        || !instructions
+                ) {
                         return res.status(403).json({
                                 message: "all fields are required",
                                 success: false
                         })
                 }
 
+                if (!status || status === undefined) {
+                        status = "Draft";
+                }
 
-                //check if instructor is
+                //check if user instructor is
                 const userId = req.user.id;
-                const instructorDetails = await User.findById(userId)
+                const instructorDetails = await User.findById(userId, {accountType: "Instructor"})
+           
 
                 if (!instructorDetails) {
                         return res.status(401).json({
@@ -47,11 +61,104 @@ exports.createCourse = async (req, res) => {
                 const thumbnailImages = await uploadImageToCloudinary(thumbnailImage, process.env.FOLDER_NAME,)
                 console.log(thumbnailImages, "thumbnail images");
                 const newCourse = await Course.create({
-                        courseName, courseDescription, whatYouWillLearn, price, tag, thumbnail: thumbnailImages.secure_url, instructor: instructorDetails._id, category: checkCategory._id
+                        courseName, courseDescription, whatYouWillLearn, price, instructions, status:status,
+                         tag, 
+                        thumbnail: thumbnailImages.secure_url, 
+                        instructor: instructorDetails._id, category: checkCategory._id
                 })
                 console.log(newCourse, "new course");
 
-             const updatedUser =   await User.findByIdAndUpdate({ _id: instructorDetails._id },
+                const updatedUser = await User.findByIdAndUpdate({ _id: instructorDetails._id },
+                        {
+                                $push: {
+                                        courses: newCourse._id
+                                }
+                        },
+                        { new: true }
+                )
+                console.log(updatedUser, "updated user");
+
+                return res.status(200).json({
+                        message: "course created successfully",
+                        success: true,
+                        response: newCourse
+                })
+
+
+        } catch (error) {
+                console.log(error);
+                return res.status(500).json({
+                        message: error.message,
+                        success: false
+                })
+        }
+}
+exports.editCourse = async (req, res) => {
+        try {
+                // fetch data 
+                const { courseName, courseDescription, whatYouWillLearn, price,
+                        tag,
+                        category,
+                        instructions,
+                        status,
+                        courseId
+                } = req.body;
+                // get thumbnail 
+
+                const { thumbnail } = req.files;
+
+                if (!courseName || !courseDescription || !whatYouWillLearn || !price
+                        // || !tag 
+                        || !thumbnail
+                        // || !category
+                        || !instructions
+                        || !courseId
+                ) {
+                        return res.status(403).json({
+                                message: "all fields are required",
+                                success: false
+                        })
+                }
+
+                if (!status || status === undefined) {
+                        status = "Draft";
+                }
+
+                //check if user instructor is
+                const userId = req.user.id;
+                console.log(userId, "userid");
+                const instructorDetails = await User.findById(userId, {accountType: "Instructor"})
+                console.log(instructorDetails, "instructor");
+           
+
+                if (!instructorDetails) {
+                        return res.status(401).json({
+                                message: "instructor detail not found",
+                                success: false
+                        })
+                }
+
+                // check given tag is valid or not 
+                const checkCategory = await Category.findById(category)
+                if (!checkCategory) {
+                        return res.status(400).json({
+                                message: "invalid category",
+                                success: false
+                        })
+                }
+
+
+                // upload image to cloudinary 
+                const thumbnailImages = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME,)
+                const newCourse = await Course.findByIdAndUpdate(courseId ,{
+                        courseName, courseDescription, whatYouWillLearn, price, instructions, status:status,
+                         tag, 
+                        thumbnail: thumbnailImages.secure_url, 
+                        instructor: instructorDetails._id, category: checkCategory._id
+                }, { new: true })
+                console.log("editCourseDetails", newCourse);
+
+                const updatedUser = await User.findByIdAndUpdate({ _id: instructorDetails._id },
                         {
                                 $push: {
                                         courses: newCourse._id
@@ -87,10 +194,10 @@ exports.showAllCourses = async (req, res) => {
                         success: true,
                         response: allCourses
                 })
-                
+
         } catch (error) {
-              console.log(error)  
-              return res.status(500).json({message: error.message, success: false});
+                console.log(error)
+                return res.status(500).json({ message: error.message, success: false });
         }
 }
 
@@ -100,23 +207,23 @@ exports.showAllCourses = async (req, res) => {
 exports.getCourseDetails = async (req, res) => {
         try {
                 // get course id 
-                const {courseId} = req.body;
+                const { courseId } = req.body;
                 // find course details 
                 const courseDetails = await Course.findById(courseId).populate({
-                      path: "instructor",
-                      populate: {
-                        path: "additionalDetails",
-                      }
+                        path: "instructor",
+                        populate: {
+                                path: "additionalDetails",
+                        }
                 }).populate("category")
-                .populate("ratingAndReviews")
-                .populate({
-                        path: "courseContent",
-                        populate: ({
-                                path: "subSection"
-                        })
-                }).exec()
+                        .populate("ratingAndReviews")
+                        .populate({
+                                path: "courseContent",
+                                populate: ({
+                                        path: "subSection"
+                                })
+                        }).exec()
 
-                if(!courseDetails){
+                if (!courseDetails) {
                         return res.status(400).json({
                                 message: `could not find course details ${courseId}`,
                                 success: false,
@@ -128,10 +235,10 @@ exports.getCourseDetails = async (req, res) => {
                         success: true,
                         response: courseDetails
                 })
-                
+
         } catch (error) {
                 console.log(error)
                 return res.status(500).json({ message: error.message, success: false });
-         
+
         }
 }
