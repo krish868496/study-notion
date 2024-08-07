af2ee2f8c8aecdc28c4e111155324ff939fbf83b
KRISHAN <krishandevraj8@gmail.com>
2024-08-07 22:00:34 +0530

    changes in profile

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router()
 
-const { deleteAccount,  getAllDetails, updateProfile, updateDisplayPicture } = require("../controllers/Profile")
+const { deleteAccount,  getAllDetails, updateProfile, updateDisplayPicture, getEnrolledCourses } = require("../controllers/Profile")
 
 const { auth } = require("../middlewares/auth");
 
 
 // router for delete accounts 
-router.delete('/deleteprofile', deleteAccount)
+router.delete('/deleteprofile', auth, deleteAccount)
 // router for update profile details
 router.put('/updateProfile', auth, updateProfile)
 // router for update profile picture 
@@ -15,7 +15,7 @@ router.put('/updateDisplayPicture', auth, updateDisplayPicture)
 // router for get user details
 router.get('/getUserDetails', auth, getAllDetails)
 // router for enroll courses 
-// router.get("/getenrolledCourses", auth, getEnrolledCourses)
+router.get("/getEnrolledCourses", auth, getEnrolledCourses)
 // router.put("/updateDisplayPicture", auth, updateDisplayPicture)
 
 
