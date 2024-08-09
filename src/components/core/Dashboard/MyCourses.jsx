73cdf96dc83766206432467d5a5030bdafff8616
KRISHAN <krishandevraj8@gmail.com>
2024-08-09 21:11:26 +0530

    added my courses

--- src/components/core/Dashboard/MyCourses.jsx
+++ src/components/core/Dashboard/MyCourses.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { fetchInstructorCourses } from '../../../services/operations/courseDetailsApi'
+import IconBtn from '../../common/IconBtn'
+import CoursesTable from './InstructorCourses/CoursesTable'
 
 const MyCourses = () => {
         const {token} = useSelector((state) => state.auth)
@@ -15,10 +17,10 @@ const MyCourses = () => {
                         }
                 }
                 fetchCourses();
-        })
+        }, [])
   return (
     <div>
-        <div>
+        <div className='text-richblack-5'>
                 <h1>My Courses</h1>
                 <IconBtn 
                 text="Add Courses"
