0987ce1b8252546580c9e69a9a3beb5b9b0bd338
KRISHAN <krishandevraj8@gmail.com>
2024-08-10 19:01:28 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
 import Cart from './components/core/Dashboard/Cart';
 import AddCourse from './components/core/Dashboard/AddCourse';
 import MyCourses from './components/core/Dashboard/MyCourses';
+import EditCourse from './components/core/Dashboard/EditCourse';
 
 function App() {
   const { user } = useSelector((state) => state.profile)
@@ -56,7 +57,8 @@ function App() {
             user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
               <>               
                 <Route path='/dashboard/add-course' element={<AddCourse />} />
-                <Route path='/dashboard/my-course' element={<MyCourses />} />
+                <Route path='/dashboard/my-courses' element={<MyCourses />} />
+                <Route path='/dashboard/edit-course/:courseId' element={<EditCourse />} />
               </>
             )
           }
