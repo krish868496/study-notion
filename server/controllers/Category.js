cb02ae85fa67edc1c45dc2551ade378d707a9e6f
KRISHAN <krishandevraj8@gmail.com>
2024-08-12 22:17:35 +0530

    changes in category controller

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Category = require("../models/Category");
 
 // create Category handler
@@ -64,12 +65,17 @@ exports.categoryPageDetails = async (req, res) => {
   try {
     // get category id
     const { categoryId } = req.body;
+    console.log(categoryId);
+      const id = new mongoose.Types.ObjectId(categoryId.trim());
     // get course for specified categoryId
-    const selectedCategory = await Category.findById(categoryId)
+    const selectedCategory = await Category.find(id)
       .populate({
-        path: "courses",
+        path: "courses",  
         match: {status: "Published"},
         populate: "ratingAndReviews",
+        populate: {
+          path: "Instructor"
+        }
       })
       .exec();
       
@@ -79,7 +85,9 @@ exports.categoryPageDetails = async (req, res) => {
         success: false,
       });
     }
-    const categoriesExceptSelected = await Category.find({_id: {$ne: categoryId}})
+
+    const categoriesExceptSelected = await Category.find({_id: {$ne: id}})
+    console.log(categoriesExceptSelected, "excluded categories");
     const differentCategories = Category.find(categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)], _id)
       .populate({
         path: "courses",
