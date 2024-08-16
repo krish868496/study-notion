631da7cf945973685fd2e1bab3fdbcf440556c38
KRISHAN <krishandevraj8@gmail.com>
2024-08-16 09:17:04 +0530

    added change profile picture file

---
+++ src/components/core/Dashboard/Settings/ChangeProfilePicture.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ChangeProfilePicture = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between rounded-md border-[1px] 
+       border-richblack-700 bg-richblack-800  text-richblack-5"></div>
+      <div className="flex items-center gap-x-4">
+        <img src="" alt="" />
+        <div className="space-y-2">
+          <p>Change Profile Picture</p>
+        </div>
+      </div>
+     
+    </div>
+  )
+}
+
+export default ChangeProfilePicture
