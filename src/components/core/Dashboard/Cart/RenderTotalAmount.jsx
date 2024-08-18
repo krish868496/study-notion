ca3d14c5de83e7d4547db3aa6588ac3a9593cb94
KRISHAN <krishandevraj8@gmail.com>
2024-08-18 15:21:32 +0530

    changes in render total amount

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import IconBtn from "../../../common/IconBtn";
+import { buyCourse } from "../../../../services/operations/StudentFeaturesAPI";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const RenderTotalAmount = () => {
   // const { total } =
+  const {token} = useState((state) => state.auth)
+  const {user} = useState((state) => state.profile)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const cart = []
   const handleBuyCourse = () => {
     const courses = cart.map((course) => course._id);
 //     todo: api integrate -> payment gateway 
-    console.log(courses);
+buyCourse(token, courses, user, navigate, dispatch)
   };
   return (
     <div>
