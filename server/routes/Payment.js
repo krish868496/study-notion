639aebe03d5ce7ed82501c8aee00d4f2f5fed9c1
KRISHAN <krishandevraj8@gmail.com>
2024-08-20 12:58:42 +0530

    fixed payment routes

--- server/routes/Payment.js
+++ server/routes/Payment.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {capturePayment, verifyPayment, sendPaymentSuccessEmail} = require("../controllers/Payments")
 const {isInstructor, isStudent, auth} = require("../middlewares/auth")
 
-router.post("/capturepayment", capturePayment)
+router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment", auth, isStudent, verifyPayment);
 router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
 
