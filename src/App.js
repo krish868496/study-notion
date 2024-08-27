48e71f08fd77241b10d17abfca070551c631a7c8
KRISHAN <krishandevraj8@gmail.com>
2024-08-27 15:30:28 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/common/Navbar';
-import ForgotPassword from './pages/ForgotPassword';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import UpdatePassword from './pages/VerifyOtp';
-import VerifyEmail from './pages/VerifyEmail';
-import VerifyOtp from './pages/VerifyOtp';
-import Dashboard from './pages/Dashboard';
-import Error from './pages/Error';
-import PrivateRoute from './components/common/PrivateRouter';
-import { ACCOUNT_TYPE } from './components/common/constant';
-import { useSelector } from 'react-redux';
-import ResetPassword from './pages/ResetPassword';
-import ChangePassword from './pages/ChangePassword';
-import Footer from './components/common/Footer';
-import MyProfile from './components/core/Dashboard/MyProfile';
-import Settings from './components/core/Dashboard/Settings/index';
-import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
-import Cart from './components/core/Dashboard/Cart';
-import AddCourse from './components/core/Dashboard/AddCourse';
-import MyCourses from './components/core/Dashboard/MyCourses';
-import EditCourse from './components/core/Dashboard/EditCourse';
-import Catalog from './pages/Catalog';
-import CourseDetails from './pages/CourseDetails';
-import ViewCourse from './pages/ViewCourse';
-import VideoDetails from './components/core/viewCourse/VideoDetails';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/common/Navbar";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import UpdatePassword from "./pages/VerifyOtp";
+import VerifyEmail from "./pages/VerifyEmail";
+import VerifyOtp from "./pages/VerifyOtp";
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
+import PrivateRoute from "./components/common/PrivateRouter";
+import { ACCOUNT_TYPE } from "./components/common/constant";
+import { useSelector } from "react-redux";
+import ResetPassword from "./pages/ResetPassword";
+import ChangePassword from "./pages/ChangePassword";
+import Footer from "./components/common/Footer";
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Settings from "./components/core/Dashboard/Settings/index";
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
+import Cart from "./components/core/Dashboard/Cart";
+import AddCourse from "./components/core/Dashboard/AddCourse";
+import MyCourses from "./components/core/Dashboard/MyCourses";
+import EditCourse from "./components/core/Dashboard/EditCourse";
+import Catalog from "./pages/Catalog";
+import CourseDetails from "./pages/CourseDetails";
+import ViewCourse from "./pages/ViewCourse";
+import VideoDetails from "./components/core/viewCourse/VideoDetails";
 
 function App() {
-  const { user } = useSelector((state) => state.profile)
+  const { user } = useSelector((state) => state.profile);
   // console.log(user.accountType);
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
@@ -57,7 +57,10 @@ function App() {
 
           {user?.accountType === ACCOUNT_TYPE.STUDENT && (
             <>
-              <Route path='/dashboard/view-course/:courseId/section/:sectionId/sub-section/:subSectionId' element={<VideoDetails />} />
+              <Route
+                path="/dashboard/view-course/:courseId/section/:sectionId/sub-section/:subSectionId"
+                element={<VideoDetails />}
+              />
               <Route
                 path="/dashboard/enrolled-courses"
                 element={<EnrolledCourses />}
@@ -78,21 +81,25 @@ function App() {
           )}
         </Route>
 
-
-        <Route element={
-          <PrivateRoute>
-            <ViewCourse />
-          </PrivateRoute>
-        }
-        >
-          {
-            user?.accountType === ACCOUNT_TYPE.STUDENT && (
-              <>
-                <Route path="/view-course/:courseId/section/:sectionId/sub-section/:subSectionId" element={<VideoDetails />} />
-                <Route path="/dashboard/enrolled-course" element={<EnrolledCourses />} />
-              </>
-            )
+        <Route
+          element={
+            <PrivateRoute>
+              <ViewCourse />
+            </PrivateRoute>
           }
+        >
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route
+                path="/view-course/:courseId/section/:sectionId/sub-section/:subSectionId"
+                element={<VideoDetails />}
+              />
+              <Route
+                path="/dashboard/enrolled-course"
+                element={<EnrolledCourses />}
+              />
+            </>
+          )}
         </Route>
 
         <Route path="*" element={<Error />} />
