dad8f84c44443b725e76807355d9104418970428
KRISHAN <krishandevraj8@gmail.com>
2024-12-15 17:38:02 +0530

    profile changes

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,20 +1,44 @@
 const mongoose = require("mongoose");
 
 const ProfileSchema = new mongoose.Schema({
-        gender:{
-                type:String
-        },
-        dateOfBirth:{
-                type: String
-        },
-        about: {
-                type: String,
-                trim: true
-        },
-        contactNumber: {
-                type: Number,
-                trim: true
-        }
-})
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: "Please enter a valid email address.",
+    },
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female"],
+    // required: true, 
+  },
+  dateOfBirth: {
+    type: String,
+  },
+  // about: {
+  //         type: String,
+  //         trim: true
+  // },
+  contactNumber: {
+    type: Number,
+    trim: true,
+  },
+});
 
 module.exports = mongoose.model("Profile", ProfileSchema)
